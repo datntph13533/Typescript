330f3434412c1fb567dd8e66f504d9a44c70e539
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-03-08 22:30:01 +0700

    update

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,14 @@
 "use strict";
-function sum(a, b) {
-    return a + b;
-}
-const tong = sum(4, 5);
-console.log(tong);
+// function sum(a:number, b:number){
+//     return a + b;
+// }
+// const tong = sum(4,5);
+// console.log(tong);
+const person = {
+    name: "Dat",
+    age: 20
+};
+const persons = [
+    { id: 1, name: "dat", age: 20 },
+    { id: 2, name: "kien", age: 20 },
+];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,38 @@
-function sum(a:number, b:number){
-    return a + b;
+// function sum(a:number, b:number){
+//     return a + b;
+// }
+// const tong = sum(4,5);
+// console.log(tong);
+
+// let number: number = 5;
+// let number1: number = 2.8;
+// let phrase: string = "Result is";
+// let permit: boolean = true;
+
+// const result = number1 +number;
+// if(permit){
+//     console.log(phrase + result);
+// }else{
+//     console.log('not show result');
+    
+// }
+
+type TPerson ={
+    id: number;
+    name: string;
+    age: number;
+    status?: boolean;
 }
-const tong = sum(4,5);
-console.log(tong);
+
+const person: TPerson ={
+    id: 1,
+    name: "Dat",
+    age: 20
+}
+
+const persons: TPerson[] = [
+    {id: 1, name: "dat", age: 20},
+    {id: 2, name: "kien", age: 20},
+]
+
+console.log(person.name);
